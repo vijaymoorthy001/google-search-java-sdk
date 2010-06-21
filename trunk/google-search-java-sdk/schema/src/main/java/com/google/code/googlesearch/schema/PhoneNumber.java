d32a9f8ddf3585cca9919c88edb1aec3c10741e3
nabeelmukhtar <nabeelmukhtar@22fa4a1a-0a79-0560-07f3-5e6ebc56a2a7>
2010-06-21 15:15:16 +0000

    Completed the schema

--- trunk/google-search-java-sdk/schema/src/main/java/com/google/code/googlesearch/schema/PhoneNumber.java
+++ trunk/google-search-java-sdk/schema/src/main/java/com/google/code/googlesearch/schema/PhoneNumber.java
@@ -7,5 +7,32 @@ package com.google.code.googlesearch.schema;
  * The Class PhoneNumber.
  */
 public class PhoneNumber {
+	
+	private PhoneNumberType type;
+	private String number;
+	/**
+	 * @return the type
+	 */
+	public PhoneNumberType getType() {
+		return type;
+	}
+	/**
+	 * @param type the type to set
+	 */
+	public void setType(PhoneNumberType type) {
+		this.type = type;
+	}
+	/**
+	 * @return the number
+	 */
+	public String getNumber() {
+		return number;
+	}
+	/**
+	 * @param number the number to set
+	 */
+	public void setNumber(String number) {
+		this.number = number;
+	}
 
 }
