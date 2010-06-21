7824200d04a54eeb8410aef498d0d4cf5e1b4c03
nabeelmukhtar <nabeelmukhtar@22fa4a1a-0a79-0560-07f3-5e6ebc56a2a7>
2010-06-21 15:15:16 +0000

    Completed the schema

---
+++ google-search-java-sdk/schema/src/main/java/com/google/code/googlesearch/schema/PhoneNumberType.java
@@ -0,0 +1,52 @@
+package com.google.code.googlesearch.schema;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.google.code.googlesearch.common.ValueEnum;
+
+public enum PhoneNumberType implements ValueEnum {
+	MAIN("main"), FAX("fax"), MOBILE("mobile"), DATA("data");
+	
+    /** The Constant stringToEnum. */
+	private static final Map<String, PhoneNumberType> stringToEnum = new HashMap<String, PhoneNumberType>();
+
+	static { // Initialize map from constant name to enum constant
+		for (PhoneNumberType op : values()) {
+			stringToEnum.put(op.value(), op);
+		}
+	}
+	
+    /** The value. */
+    private final String value;
+    
+    /**
+     * Instantiates a new patent status.
+     * 
+     * @param value the value
+     */
+    PhoneNumberType(String value) {
+        this.value = value;
+    }
+
+	/* (non-Javadoc)
+	 * @see com.google.code.googlesearch.common.ValueEnum#value()
+	 */
+	@Override
+	public String value() {
+		return value;
+	}
+	
+	/**
+	 * From value.
+	 * 
+	 * @param value the value
+	 * 
+	 * @return the patent status
+	 */
+	public static PhoneNumberType fromValue(String value) {
+		return stringToEnum.get(value);
+	}
+
+
+}
